d2199a3d277726fa6d18248aaea137350e053fc2
Vigneshkumar G <vigneshkumarg18@gmail.com>
2019-02-05 23:44:43 +0530

    UIColor COnstants

--- Gists.playground/Contents.swift
+++ Gists.playground/Contents.swift
@@ -1,5 +1,50 @@
 import UIKit
 
-let redWithAlpha = UIColor(hexString: "#00ff00ff")
+let redWithAlpha = UIColor(hexString: "#F0F8FFFF")
 
-let redWithoutAlpha = UIColor(hexString: "#ffff00ab")
+let redWithoutAlpha = UIColor(hexString: "#FAEBD7FF")
+
+
+let path = Bundle.main.path(forResource: "HTMLColors", ofType: "strings")!
+
+let data = Data(contentsOf: URL.init(fileURLWithPath: path))
+
+let string = String(data: data, encoding: .utf8)!
+
+var enumString = "enum HTMLColor {\n"
+
+var cases = [String]()
+
+var values = [String]()
+
+
+for (index,line) in string.split(separator: ",").enumerated()
+{
+    let trimSet = CharacterSet.init(charactersIn: "\" \n")
+    
+    let first = line.split(separator: ":")[0]
+    let second = line.split(separator: ":")[1]
+    let firstTrimed = first.trimmingCharacters(in: trimSet)
+    let secondTrimed = second.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    let caseString = "case \(firstTrimed) = \(secondTrimed)\n"
+    cases.append(firstTrimed)
+    values.append(secondTrimed)
+}
+
+for caseItem in cases {
+    enumString.append("case \(caseItem)\n")
+}
+
+enumString.append("func getHexString() -> String { \n")
+enumString.append("switch self { \n")
+for (caseItem,val) in zip(cases, values) {
+    enumString.append("case .\(caseItem):\n")
+    enumString.append("return \(val)\n")
+}
+enumString.append("}")
+enumString.append("}\n")
+enumString.append("}\n")
+
+print(enumString)
+
+UIColor(hexString: UIColor.HTMLColor.aqua.getHexString())
